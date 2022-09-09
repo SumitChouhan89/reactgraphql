39c70b1169c8fc0544d6eb8496a1d527fa0580c3
Sumit Chauhan <sumit.chouhan@technoidentity.com>
2022-09-09 18:22:10 +0530

    Initial Commit

---
+++ src/RepoInfo.js
@@ -0,0 +1,71 @@
+const RepoInfo = ({ repo }) => {
+  let license;
+  switch (repo.licenseInfo?.spdxId) {
+    case undefined:
+      license = (
+        <span
+          className="px-1 py-0 ms-1 d-inline-block btn btn-sm btn-danger"
+          style={{ fontSize: ".6em" }}
+        >
+          NO LICENSE
+        </span>
+      );
+      break;
+    case "NOASSERTION":
+      license = (
+        <span
+          className="px-1 py-0 ms-1 d-inline-block btn btn-sm btn-warning"
+          style={{ fontSize: ".6em" }}
+        >
+          {repo.licenseInfo.spdxId}
+        </span>
+      );
+      break;
+    default:
+      license = (
+        <span
+          className="px-1 py-0 ms-1 d-inline-block btn btn-sm btn-outline-success"
+          style={{ fontSize: ".6em" }}
+        >
+          {repo.licenseInfo.spdxId}
+        </span>
+      );
+  }
+  return (
+    <li className="list-group-item" key={repo.id.toString()}>
+      <div className="d-flex justify-content-between align-items-center">
+        <div className="d-flex flex-column">
+          <a
+            className="h5 mb-0 text-decoration-none"
+            href={repo.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {repo.name}
+          </a>
+          <p className="small">
+            {repo.description === null
+              ? "No description of this repo"
+              : repo.description}
+          </p>
+        </div>
+        <div className="text-nowrap ms-3">
+          {license}
+          <span
+            style={{ fontSize: ".6em" }}
+            className={
+              "px-1 py-1 ms-1 d-inline-block btn btn-sm " +
+              (repo.viewerSubscription === "SUBSCRIBED"
+                ? "btn-success"
+                : "btn-outline-secondary")
+            }
+          >
+            {repo.viewerSubscription}
+          </span>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default RepoInfo;
